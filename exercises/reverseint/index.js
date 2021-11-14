cdb8db1db084b57ea592b36ec589c20ced7203f9
Christopher Morris <chrispatmorris@gmail.com>
2021-11-14 18:42:11 -0500

    reverse int

--- exercises/reverseint/index.js
+++ exercises/reverseint/index.js
@@ -8,6 +8,29 @@
 //   reverseInt(-15) === -51
 //   reverseInt(-90) === -9
 
-function reverseInt(n) {}
+function reverseIntV1(n) {
+
+    const arr = n.toString().split('');
+    let isMinus = arr.includes('-');
+
+    if (isMinus) {
+        arr.splice(arr.indexOf('-'), 1);
+    }
+
+    const str = arr.join('');
+    let rev = str.split('').reverse().join('');
+
+    if (isMinus) {
+        rev = '-' + rev;
+    }
+
+    return parseInt(rev);
+}
+
+function reverseInt(n) {
+    const rev = n.toString().split('').reverse().join('');
+    return parseInt(rev) * Math.sign(n);
+}
+
 
 module.exports = reverseInt;
