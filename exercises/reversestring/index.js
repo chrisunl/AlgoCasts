80b722746bd44f11ad4ce02299ac39568deecd75
Christopher Morris <chrispatmorris@gmail.com>
2021-11-13 21:44:44 -0500

    str reverse

--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -6,6 +6,27 @@
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
-function reverse(str) {}
+// reduce array to a single result, iterate over elements
+function reverse(str) {
+    // 2nd param is start value, func->acc is result of previous invocation, 
+    // func->char is current char from arr
+    return str.split('').reduce((acc, char) => char + acc, '');
+}
+
+function reverseV1(str) {
+    // trivial
+    return str.split('').reverse().join('');
+}
+
+// manual
+function reverseV2(str) {
+    let rev = "";
+    //es2015 'for of' loop
+    for (let char of str) {
+       rev = char + rev;
+    }
+
+    return rev;
+}
 
 module.exports = reverse;
