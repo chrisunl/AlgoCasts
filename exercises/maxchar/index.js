e891b7c9afb2609e0fee30f73591a45a67759328
Christopher Morris <chrispatmorris@gmail.com>
2021-12-11 22:49:44 -0500

    maxchar impl v1

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,6 +5,19 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+//V1
+function maxChar(str) {
+    const charCount = new Map();
+
+    for (const char of str) {;
+        let count = charCount.has(char) ? charCount.get(char) : 0;
+        charCount.set(char, ++count);
+    }
+
+    const arr = Array.from(charCount);
+    arr.sort((a, b) => { return a[1] < b[1] ? 1 : -1; });
+
+    return arr[0][0];
+}
 
 module.exports = maxChar;
