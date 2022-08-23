c9a4da83658ca765f9528e2dbbcbbae4e2763bd9
Christopher Morris <chrispatmorris@gmail.com>
2022-08-22 20:45:23 -0400

    anagrams; ;

--- exercises/anagrams/index.js
+++ exercises/anagrams/index.js
@@ -8,6 +8,17 @@
 //   anagrams('RAIL! SAFETY!', 'fairy tales') --> True
 //   anagrams('Hi there', 'Bye there') --> False
 
-function anagrams(stringA, stringB) {}
+function cleanString(str) {
+    return str
+      .replace(/[^\w]/g, '')
+      .toLowerCase()
+      .split('')
+      .sort()
+      .join('');
+  }
+
+function anagrams(stringA, stringB) {
+    return cleanString(stringA) == cleanString(stringB)
+}
 
 module.exports = anagrams;
