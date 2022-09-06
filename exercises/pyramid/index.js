c02ba545292e4d299e2a6a2984c7695a88bbfcaf
Christopher Morris <chrispatmorris@gmail.com>
2022-09-06 15:46:01 -0400

    pyramid, 1st solution, loops

--- exercises/pyramid/index.js
+++ exercises/pyramid/index.js
@@ -11,9 +11,44 @@
 //       '###'
 //   pyramid(3)
 //       '  #  '
-//       ' ### '
+//       ' ### ' 5 - 3 = 2
 //       '#####'
 
-function pyramid(n) {}
+  //  '---x---' 0 + 1 
+  //  '--xxx--' 1 + 2
+  //  '-xxxxx-' 2 + 3
+  //  'xxxxxxx' 3 + 4
+
+// Width = n + n - 1
+
+function pyramid(n) {
+    
+    for (let row = 0; row < n; row++) {
+        let line = '';
+        const numColumns = n + (n - 1);
+        const numHashes = row + (row + 1);
+        const numSpacesOnLeft = (numColumns - numHashes) / 2;
+        let hashes = 0;
+        let spaces = 0;
+        for (let col = 0; col < numColumns; col++) {
+            let symbol = '';
+            if (spaces < numSpacesOnLeft) {
+                symbol = ' ';
+                spaces++;
+            } else if (hashes < numHashes) {
+                symbol = '#';
+                hashes++;
+            } else {
+                // symetrical so whatever space is left on the right (if any) after all the hashes must be spaces
+                symbol = ' ';
+                hashes++;
+            }
+
+            line += symbol;
+        }
+        console.log(line);
+    }
+
+}
 
 module.exports = pyramid;
