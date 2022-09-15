22f8c97025d0ded5e3808eb047c869b4d22ef74d
Christopher Morris <chrispatmorris@gmail.com>
2022-09-15 11:05:14 -0400

    matrix spiral udemy solution

--- exercises/matrix/index.js
+++ exercises/matrix/index.js
@@ -15,78 +15,49 @@
 //     [11, 16, 15, 6],
 //     [10,  9,  8, 7]]
 
-// for i <= n
-// while i < n top row left to right
-// while i < n*2 last col top to bottom
-// while i < n*3 last row right to left
-// while i < n*4 first col bottom to top-1
-// while i < n*5 (top row) + 1 left to (right - 1)
-// while i < n*6 (top row) + 2 (right - 1) to left + 1
-
-// rowIndex = 0, colIndex = 0
-// traverse = col
-// direction = forward
-
-// for i <= n
-// while i < n top row left to right, 
-// while i < n*2 last col top to bottom
-// while i < n*3 last row right to left
-// while i < n*4 first col bottom to top-1
-// while i < n*5 (top row) + 1 left to (right - 1)
-// while i < n*6 (top row) + 2 (right - 1) to left + 1
-
-
 
 function matrix(n) {
-    const outer = new Array(n);
+    const results = new Array(n);
 
     for (let i = 0; i < n; i++) {
-        outer[i] = new Array(n);
+        results[i] = [];
     }
 
-    let number = 1;
-    let traverseRow = true;
-    let forward = true;
-    let row = 0;
-    let col = 0;
-    let iteration = 1;
+    let counter = 1;
+    let startColumn = 0
+    let endColumn = n - 1;
+    let startRow = 0;
+    let endRow = n - 1;
 
-    while (number <= n*n) {
-        outer[row][col] = number;
-        console.log("number:", number);
-        console.log("limit:", n*iteration);
-
-        console.log("row:", row);
-        console.log("col:", col);
-        if (number <= n*iteration) {
-
-            if (traverseRow) {
-                if (forward) {
-                    if (col < n && outer[row][col + 1] === undefined)
-                    col++;   
-                } else {
-                    if (col > 0 &&outer[row][col - 1] === undefined)
-                    col--;   
-                }
-            } else {
-                if (forward) {
-                    if (row < n && outer[row + 1][col] === undefined)
-                    row++;   
-                } else {
-                    if (row > 0 && outer[row - 1][col] === undefined)
-                    row--;   
-                }
-            }
-        } else {
-            iteration++
-            traverseRow = !traverseRow;
-            forward = !forward;
+    while (startColumn <= endColumn && startRow <= endRow) {
+        //Top Row
+        for (let i = startColumn; i <= endColumn; i++) {
+            results[startRow][i] = counter;
+            counter++;
         }
-
-        number++;
+        startRow++;
+        //Right Column
+        for (let j = startRow; j <= endRow; j++) {
+            results[j][endColumn] = counter;
+            counter++;
+        }
+        endColumn--;
+        // Bottom Row
+        for (let k = endColumn; k >=startColumn; k--) {
+            results[endRow][k] = counter;
+            counter++;
+        }
+        endRow--;
+        // Start Column
+        for (let l = endRow; l >=startRow; l--) {
+            results[l][startColumn] = counter;
+            counter++;
+        }
+        startColumn++;
     }
-    console.log("outer: ", outer);
-    return outer;
+    
+    console.log("results: ", results);
+    return results;
 }
 
 module.exports = matrix;
