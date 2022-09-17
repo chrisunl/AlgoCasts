f73f964b5f281e2148b52f077dac06214f60a3dd
Christopher Morris <chrispatmorris@gmail.com>
2022-09-17 13:40:15 -0400

    fib recursion with caching

--- exercises/fib/index.js
+++ exercises/fib/index.js
@@ -6,15 +6,50 @@
 //  [0, 1, 1, 2, 3, 5, 8, 13, 21, 34]
 // forms the first ten entries of the fibonacci series.
 // Example:
+//   fib(5) === 5
 //   fib(4) === 3
 //   fib(3) === 2
 
-function fib(n) {
+
+// recursive solution has exponential time complexity (!) O(2^n) (calls itself twice for each N)
+// function fib(n) {
+//     if (n <= 2) {
+//         return 1; 
+//     } else {
+//         return fib(n - 1) + fib(n - 2);
+//     }
+// }
+
+// recursive solution with caching, O(n), fixes exponential time complexity of simple recursive solution
+function fib(n, cache = null) {
+
     if (n <= 2) {
         return 1; 
+    }
+
+    if (!cache) {
+        cache = new Map();
+    }
+
+    if (cache.has(n)) {
+        return cache.get(n);
     } else {
-        return fib(n - 1) + fib(n - 2);
+        const result = fib(n - 1, cache) + fib(n - 2, cache)
+        cache.set(n, result);
+        return result;
     }
 }
+
+// fib iterative, linear O(n)
+// function fib(n) {
+//     const fibArr = [0,1];
+//     for (let i = 2; i <= n; i++) {
+//         if (fibArr.length > 1) {
+//             fibArr.push(fibArr[i - 1] + fibArr[i - 2]);
+//         }       
+//     }
+
+//     return fibArr[n];
+// }
 
 module.exports = fib;
