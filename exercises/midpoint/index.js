0a3e7aeb0100a649fa67b01c655cf4716dc9f18b
Christopher Morris <chrispatmorris@gmail.com>
2022-10-10 16:30:03 -0400

    midpoint solution

--- exercises/midpoint/index.js
+++ exercises/midpoint/index.js
@@ -12,6 +12,33 @@
 //   l.insertLast('c')
 //   midpoint(l); // returns { data: 'b' }
 
-function midpoint(list) {}
+
+function midpoint(list) {
+    let slow = list.getFirst();
+    let fast = list.getFirst();
+    // fast travels twice as fast as slow so when fast hits the end (or end - 1) then slow is halfway there, thus at the midpoint
+    while (fast.next && fast.next.next) {
+        slow = slow.next;
+        fast = fast.next.next;
+    }
+
+    return slow;
+}
+
+// array impl, kind of cheating, array has a counter
+// function midpoint(list) {
+//     let current = list.getFirst();
+//     const arr = [];
+//     while (current) {
+//         arr.push(current);
+//         current = current.next;
+//     }
+
+//     if (arr.length % 2 == 0) {
+//         return arr[(arr.length / 2) -1];
+//     } else {
+//         return arr[Math.floor(arr.length / 2)];
+//     }
+// }
 
 module.exports = midpoint;
